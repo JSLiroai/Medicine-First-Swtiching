48f577136c444395853c8e18afa609a99371afbf
Sung-geun-Choi <99890543+Sung-geun-Choi@users.noreply.github.com>
2022-05-15 14:49:36 +0900

    SearchActivity 자동완성기능

---
+++ MedicineFirstSwitching/app/src/main/java/com/example/medicinefirstswitching/Searching/SearchItem.java
@@ -0,0 +1,13 @@
+package com.example.medicinefirstswitching.Searching;
+
+public class SearchItem {
+    private String text;
+
+    public SearchItem(String data) {
+        text = data;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
