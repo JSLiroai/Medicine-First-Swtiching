316acbd8549c9a75a6db38aad4370ed9c7a4c224
Sung-geun-Choi <99890543+Sung-geun-Choi@users.noreply.github.com>
2022-06-12 15:45:08 +0900

    ResultActivity - ReviewActivity시작 버튼 추가

--- MedicineFirstSwitching/app/src/main/java/com/example/medicinefirstswitching/ReviewActivity.java
+++ MedicineFirstSwitching/app/src/main/java/com/example/medicinefirstswitching/ReviewActivity.java
@@ -9,6 +9,6 @@ public class ReviewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reiview);
+        setContentView(R.layout.activity_review);
     }
 }
